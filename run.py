1864cb7f484cd6de2adbdadfb6f4a8b9437f7715
jm <jm@gmail.com>
2025-03-24 14:16:16 +0400

    更新依赖

--- run.py
+++ run.py
@@ -12,70 +12,71 @@ def main():
     torch.manual_seed(fix_seed)
     np.random.seed(fix_seed)
 
-    parser = argparse.ArgumentParser(description='Autoformer & Transformer family for Time Series Forecasting')
+    parser = argparse.ArgumentParser(description='Autoformer & Transformer 系列用于时间序列预测')
 
-    # basic config
-    parser.add_argument('--is_training', type=int, required=True, default=1, help='status')
-    parser.add_argument('--model_id', type=str, required=True, default='test', help='model id')
+    # 基本配置
+    parser.add_argument('--is_training', type=int, required=True, default=1, help='状态（是否进行训练）')
+    parser.add_argument('--model_id', type=str, required=True, default='test', help='模型标识')
     parser.add_argument('--model', type=str, required=True, default='Autoformer',
-                        help='model name, options: [Autoformer, Informer, Transformer]')
+                        help='模型名称，选项：[Autoformer, Informer, Transformer]')
 
-    # data loader
-    parser.add_argument('--data', type=str, required=True, default='ETTm1', help='dataset type')
-    parser.add_argument('--root_path', type=str, default='./data/ETT/', help='root path of the data file')
-    parser.add_argument('--data_path', type=str, default='ETTh1.csv', help='data file')
+    # 数据加载器
+    parser.add_argument('--data', type=str, required=True, default='ETTm1', help='数据集类型')
+    parser.add_argument('--root_path', type=str, default='./data/ETT/', help='数据文件的根路径')
+    parser.add_argument('--data_path', type=str, default='ETTh1.csv', help='数据文件')
     parser.add_argument('--features', type=str, default='M',
-                        help='forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, S:univariate predict univariate, MS:multivariate predict univariate')
-    parser.add_argument('--target', type=str, default='OT', help='target feature in S or MS task')
+                        help='预测任务，选项：[M, S, MS]; M:多变量预测多变量, S:单变量预测单变量, MS:多变量预测单变量')
+    parser.add_argument('--target', type=str, default='OT', help='在 S 或 MS 任务中的目标特征')
     parser.add_argument('--freq', type=str, default='h',
-                        help='freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h')
-    parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
+                        help='时间特征编码的频率，选项：[s:秒级, t:分钟级, h:小时级, d:日级, b:工作日, w:周级, m:月级]，也可以使用更详细的频率如 15min 或 3h')
+    parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='模型检查点的位置')
 
-    # forecasting task
-    parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
-    parser.add_argument('--label_len', type=int, default=48, help='start token length')
-    parser.add_argument('--pred_len', type=int, default=96, help='prediction sequence length')
+    # 预测任务
+    parser.add_argument('--seq_len', type=int, default=96, help='输入序列长度')
+    parser.add_argument('--label_len', type=int, default=48, help='起始标记长度')
+    parser.add_argument('--pred_len', type=int, default=96, help='预测序列长度')
 
-    # model define
-    parser.add_argument('--bucket_size', type=int, default=4, help='for Reformer')
-    parser.add_argument('--n_hashes', type=int, default=4, help='for Reformer')
-    parser.add_argument('--enc_in', type=int, default=7, help='encoder input size')
-    parser.add_argument('--dec_in', type=int, default=7, help='decoder input size')
-    parser.add_argument('--c_out', type=int, default=7, help='output size')
-    parser.add_argument('--d_model', type=int, default=512, help='dimension of model')
-    parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
-    parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
-    parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
-    parser.add_argument('--d_ff', type=int, default=2048, help='dimension of fcn')
-    parser.add_argument('--moving_avg', type=int, default=25, help='window size of moving average')
-    parser.add_argument('--factor', type=int, default=1, help='attn factor')
+    # 模型定义
+    parser.add_argument('--bucket_size', type=int, default=4, help='适用于 Reformer 的桶大小')
+    parser.add_argument('--n_hashes', type=int, default=4, help='适用于 Reformer 的哈希数量')
+    parser.add_argument('--enc_in', type=int, default=7, help='编码器输入大小')
+    parser.add_argument('--dec_in', type=int, default=7, help='解码器输入大小')
+    parser.add_argument('--c_out', type=int, default=7, help='输出大小')
+    parser.add_argument('--d_model', type=int, default=512, help='模型维度')
+    parser.add_argument('--n_heads', type=int, default=8, help='头的数量')
+    parser.add_argument('--e_layers', type=int, default=2, help='编码器层数')
+    parser.add_argument('--d_layers', type=int, default=1, help='解码器层数')
+    parser.add_argument('--d_ff', type=int, default=2048, help='全连接层维度')
+    parser.add_argument('--moving_avg', type=int, default=25, help='移动平均窗口大小')
+    parser.add_argument('--factor', type=int, default=1, help='注意力因子')
     parser.add_argument('--distil', action='store_false',
-                        help='whether to use distilling in encoder, using this argument means not using distilling',
+                        help='是否在编码器中使用蒸馏，使用此参数表示不使用蒸馏',
                         default=True)
-    parser.add_argument('--dropout', type=float, default=0.05, help='dropout')
+    parser.add_argument('--dropout', type=float, default=0.05, help='丢弃率')
     parser.add_argument('--embed', type=str, default='timeF',
-                        help='time features encoding, options:[timeF, fixed, learned]')
-    parser.add_argument('--activation', type=str, default='gelu', help='activation')
-    parser.add_argument('--output_attention', action='store_true', help='whether to output attention in encoder')
-    parser.add_argument('--do_predict', action='store_true', help='whether to predict unseen future data')
+                        help='时间特征编码，选项：[timeF, fixed, learned]')
+    parser.add_argument('--activation', type=str, default='gelu', help='激活函数')
+    parser.add_argument('--output_attention', action='store_true', help='是否在编码器中输出注意力')
+    parser.add_argument('--do_predict', action='store_true', help='是否预测未见过的未来数据')
 
-    # optimization
-    parser.add_argument('--num_workers', type=int, default=10, help='data loader num workers')
-    parser.add_argument('--itr', type=int, default=2, help='experiments times')
-    parser.add_argument('--train_epochs', type=int, default=10, help='train epochs')
-    parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
-    parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
-    parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
-    parser.add_argument('--des', type=str, default='test', help='exp description')
-    parser.add_argument('--loss', type=str, default='mse', help='loss function')
-    parser.add_argument('--lradj', type=str, default='type1', help='adjust learning rate')
-    parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
+    # 优化
+    parser.add_argument('--num_workers', type=int, default=10, help='数据加载器的工作线程数')
+    parser.add_argument('--itr', type=int, default=2, help='实验次数')
+    parser.add_argument('--train_epochs', type=int, default=10, help='训练轮数')
+    parser.add_argument('--batch_size', type=int, default=32, help='训练输入数据的批量大小')
+    parser.add_argument('--patience', type=int, default=3, help='提前停止的耐心值')
+    parser.add_argument('--learning_rate', type=float, default=0.0001, help='优化器学习率')
+    parser.add_argument('--des', type=str, default='test', help='实验描述')
+    parser.add_argument('--loss', type=str, default='mse', help='损失函数')
+    parser.add_argument('--lradj', type=str, default='type1', help='调整学习率')
+    parser.add_argument('--use_amp', action='store_true', help='是否使用自动混合精度训练', default=False)
 
     # GPU
-    parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
-    parser.add_argument('--gpu', type=int, default=0, help='gpu')
-    parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
-    parser.add_argument('--devices', type=str, default='0,1,2,3', help='device ids of multile gpus')
+    parser.add_argument('--use_gpu', type=bool, default=True, help='是否使用 gpu')
+    parser.add_argument('--gpu', type=int, default=0, help='gpu 编号')
+    parser.add_argument('--use_multi_gpu', action='store_true', help='是否使用多个 gpus', default=False)
+    parser.add_argument('--devices', type=str, default='0,1,2,3', help='多 gpu 的设备编号')
+
 
     args = parser.parse_args()
 
